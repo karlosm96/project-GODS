ceb584901446468e3fe2561b50889a3dc756bfee
CarlosM <karlos_m96@hotmail.com>
2021-10-21 03:31:13 -0300

    delete send_email.js

--- pages/send_email.js
+++
@@ -1,7 +0,0 @@
-const submitButtom = document.getElementById("submit");
-const input = document.getElementById("playerName");
-
-
-input.addEventListener("change", (e)=>{
-    console.log(e.currentTarget.value);
-})
